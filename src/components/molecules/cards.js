a7bd3213cb02c8e92daaab2d4841c1b2fbae1b34
Vinay Kumar Singh <vin05_singh@yahoo.com>
2020-07-03 12:51:58 +0530

    update cards animation

--- src/components/molecules/cards.js
+++ src/components/molecules/cards.js
@@ -12,17 +12,30 @@ const CardTitle = styled.h4`
 `;
 const CardText = styled.div`
 	margin-bottom: 5px;
-	display: inline-block;
+	display: block;
 `;
 const CardTextBold = styled.span`
 	margin-right: 5px;
 	font-weight: bold;
 `;
+const Inner = styled.div`
+	overflow: hidden;
+	img {
+		-webkit-transition: all 1.5s ease;
+		-moz-transition: all 1.5s ease;
+		transition: all 1.5s ease;
+	}
+	&:hover img {
+		transform: scale(1.5);
+	}
+`;
 
 const Card = (props) => {
 	return (
 		<div className='card'>
-			<img className='card-img-top img-fluid' src={props.data.image} alt='Card image cap' width='400' />
+			<Inner>
+				<img className='card-img-top img-fluid' src={props.data.image} alt='Card image cap' width='400' />
+			</Inner>
 			<div className='card-body'>
 				<CardTitle>{props.data.name}</CardTitle>
 				<CardText>
--- src/components/organisms/resultList.js
+++ src/components/organisms/resultList.js
@@ -3,10 +3,10 @@ import Cards from '../molecules/cards';
 
 const ResultList = (props) => {
 	return (
-		<div className='row'>
+		<div className='row mb-2'>
 			{props.resultItem.map((data, index) => {
 				return (
-					<div className='col-sm-6 col-md-4 col-lg-3 mt-4' key={index}>
+					<div className='col-sm-6 col-md-4 col-lg-4 mt-4' key={index}>
 						<Cards data={data} />
 					</div>
 				);
